d2a49c3d2780cd9133bfcc29589e8c0da9c31df8
Samuel Silva <samus355@gmail.com>
2020-08-12 16:30:00 -0300

    finish menu page

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
 
+import './styles.css';
+
+import Show from '../../assets/images/show.jpg';
+import Bar from '../../assets/images/bar.jpg';
+import Pen from '../../assets/icons/caneta.svg';
+import Search from '../../assets/icons/lupa.svg';
+
 function Menu() {
     return(
-        <h1>Menu</h1>
+        <div id="page-menu">
+            <div id="page-menu-content">
+                <div className="logo-container">
+                    <h1>VAMOW</h1>
+                    <h5>SUA PLATAFORMA DE EVENTOS</h5>
+                    <h6>MENU</h6>
+                </div>
+                <div id="nav-menu-container">
+                    <div id="outer-frame">
+                        <div className="browse-buttons">
+                            <img src={Show} alt="show"/>
+                            <a href="">SHOWS</a>
+                        </div>
+                        <div className="browse-buttons">
+                            <img src={Bar} alt="bares"/>
+                            <a href="">BARES</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="buttons-container">
+                    <a href="" className="buscar">
+                        <img src={Search} alt="Lupa"/>
+                        Buscar evento
+                    </a>
+                    <a href="" className="criar">
+                        <img src={Pen} alt="Caneta"/>
+                        Criar evento
+                    </a>  
+                </div>
+            </div>
+        </div>
     )
 }
 
